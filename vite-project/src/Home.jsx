9c9454a821dbb4086e09c95b8d04e003a4685d54
OLEKSANDR LAZURENKO <alexprofteach@gmail.com>
2024-02-13 00:09:58 +0000

    added some styling

--- vite-project/src/Home.jsx
+++ vite-project/src/Home.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 
+import { NavLink } from 'react-router-dom'
+
 function Home() {
   return (
     <div>
+      <nav>
+        <NavLink to="/" className="nav-link" activeClassName="active">Home</NavLink>
+        <NavLink to="/about" className="nav-link" activeClassName="active">About</NavLink>
+        <NavLink to="/contact" className="nav-link" activeClassName="active">Contact</NavLink>
+      </nav>
       <h1>This is the home page</h1>
       <Link to="/about" className="link"><button>Click to view our about page</button></Link>
       <Link to="/contact" className="link"><button>Click to view our contact pag</button></Link>
